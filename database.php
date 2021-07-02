3dd3441d916c4bad5fbd4b20740da52495fefe04
jerik.garcia <86265398+jerik-unvetica@users.noreply.github.com>
2021-07-02 14:00:22 +0800

    Add files via upload

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$con= new mysqli('72.212.147.24','padbdev','luckyboy01','play-along-app')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('72.212.147.24','padbdev','luckyboy01','play_along_dev')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
 
